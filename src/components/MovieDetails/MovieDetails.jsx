d003dde77614c719d1b21c8b75e69820edf9e617
Totalinfonet <totalinfonet@gmail.com>
2023-04-02 13:02:29 +0300

    Update MovieDetails.jsx

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,50 +1,10 @@
-// import { useParams } from 'react-router-dom';
-// import { useEffect, useState } from 'react';
-// import apiService from '../../service/apiService';
-
-// const MovieDetails = () => {
-//   const { movieId } = useParams();
-//   const [movieInfo, setMovieInfo] = useState(null);
-
-//   useEffect(() => {
-//     const fetchMovieInfo = async () => {
-//       try {
-//         const data = await apiService.getMovieInfo(movieId);
-//         setMovieInfo(data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-
-//     fetchMovieInfo();
-//   }, [movieId]);
-
-//   return (
-//     <div>
-//       {movieInfo ? (
-//         <div>
-//           <h1>{movieInfo.title}</h1>
-//           <p>{movieInfo.overview}</p>
-//           <p>Release date: {movieInfo.release_date}</p>
-//           <p>Popularity: {movieInfo.popularity}</p>
-//         </div>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default MovieDetails;
-
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import apiService from '../../service/apiService';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
   const [movieInfo, setMovieInfo] = useState(null);
-  const history = useHistory();
 
   useEffect(() => {
     const fetchMovieInfo = async () => {
@@ -59,10 +19,6 @@ const MovieDetails = () => {
     fetchMovieInfo();
   }, [movieId]);
 
-  const handleGoBack = () => {
-    history.goBack();
-  };
-
   return (
     <div>
       {movieInfo ? (
@@ -71,7 +27,6 @@ const MovieDetails = () => {
           <p>{movieInfo.overview}</p>
           <p>Release date: {movieInfo.release_date}</p>
           <p>Popularity: {movieInfo.popularity}</p>
-          <button onClick={handleGoBack}>Go back</button>
         </div>
       ) : (
         <p>Loading...</p>
