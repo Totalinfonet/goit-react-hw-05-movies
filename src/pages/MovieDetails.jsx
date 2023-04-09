0744b11faedd2c6408df797b61226141413b01b0
Totalinfonet <totalinfonet@gmail.com>
2023-04-09 14:32:11 +0300

    Update MovieDetails.jsx

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams, Link, useLocation, Outlet } from 'react-router-dom';
 import { useEffect, useState, useRef, Suspense } from 'react';
 import apiService from '../service/apiService';
+import styled from '@emotion/styled';
 
 const MovieDetails = () => {
   const { movieId } = useParams();
@@ -22,27 +23,129 @@ const MovieDetails = () => {
   }, [movieId]);
 
   return (
-    <div>
-      <Link to={backLinkHref.current}>Назад</Link>
+    <Container>
+      <BackLink to={backLinkHref.current}>Назад</BackLink>
       {movieInfo ? (
-        <div>
-          <h1>{movieInfo.title}</h1>
-          <p>{movieInfo.overview}</p>
-          <p>Release date: {movieInfo.release_date}</p>
-          <p>Popularity: {movieInfo.popularity}</p>
-          <nav>
-            <Link to="cast">Акторський склад</Link>
-            <Link to="reviews">Огляди</Link>
-          </nav>
-          <Suspense fallback={<div>Loading page...</div>}>
-            <Outlet />
-          </Suspense>
-        </div>
+        <MovieInfoContainer>
+          <Poster
+            src={`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`}
+            alt={movieInfo.title}
+          />
+          <MovieInfo>
+            <Title>{movieInfo.title}</Title>
+            <Overview>{movieInfo.overview}</Overview>
+            <ReleaseDate>Release date: {movieInfo.release_date}</ReleaseDate>
+            <Popularity>Popularity: {movieInfo.popularity}</Popularity>
+            <Genres>
+              Genres: {movieInfo.genres.map(genre => genre.name).join(', ')}
+            </Genres>
+          </MovieInfo>
+        </MovieInfoContainer>
       ) : (
         <p>Loading...</p>
       )}
-    </div>
+      <Nav>
+        <NavLink to="cast">Акторський склад</NavLink>
+        <NavLink to="reviews">Огляди</NavLink>
+      </Nav>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Outlet />
+      </Suspense>
+    </Container>
   );
 };
 
 export default MovieDetails;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const BackLink = styled(Link)`
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  font-size: 1.2rem;
+  text-align: center;
+  text-decoration: none;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #555;
+    color: orange;
+  }
+`;
+
+const MovieInfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: flex-start;
+  margin-top: 1rem;
+`;
+
+const Poster = styled.img`
+  max-width: 33%;
+`;
+
+const MovieInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  margin-top: 1rem;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  margin: 1rem 0;
+`;
+
+const Overview = styled.p`
+  font-size: 1.2rem;
+  margin: 1rem 0;
+`;
+
+const ReleaseDate = styled.p`
+  font-size: 1.2rem;
+`;
+
+const Popularity = styled.p`
+  font-size: 1.2rem;
+`;
+
+const Genres = styled.p`
+  font-size: 1.2rem;
+`;
+
+const Nav = styled.nav`
+  margin-top: 1rem;
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+`;
+
+const NavLink = styled(Link)`
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  font-size: 1.2rem;
+  text-align: center;
+  text-decoration: none;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #555;
+    color: orange;
+  }
+`;
