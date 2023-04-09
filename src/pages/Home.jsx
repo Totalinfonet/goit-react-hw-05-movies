03d4fe9d2f4239231eb78529b0cce96c3deb0e1d
Totalinfonet <totalinfonet@gmail.com>
2023-04-09 13:28:56 +0300

    Added Home Styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useEffect, useState, useMemo } from 'react';
 import apiService from '../service/apiService';
+import styled from '@emotion/styled';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
@@ -21,7 +22,7 @@ const Home = () => {
   const memoizedMovies = useMemo(() => movies, [movies]);
 
   return (
-    <div>
+    <HomeWrapper>
       <h1>Trending Today</h1>
       <ul>
         {memoizedMovies.map(movie => (
@@ -30,8 +31,36 @@ const Home = () => {
           </li>
         ))}
       </ul>
-    </div>
+    </HomeWrapper>
   );
 };
 
 export default Home;
+
+const HomeWrapper = styled.div`
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
+
+  ul {
+    list-style-type: disclosure-closed;
+    margin-left: 48px;
+    padding: 0;
+
+    li {
+      margin-bottom: 0.5rem;
+    }
+
+    a {
+      color: blue;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+        font-weight: bold;
+      }
+    }
+  }
+`;
